790a03c9110305f66728e9204907c9f619249d9d
MalarzDawid <github@dawidmalarz.pl>
2022-04-03 12:09:35 +0200

    Add function to display labels

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ import streamlit as st
 
 st.title("8086 SIMULATOR")
 
-col1, col2, col3, col4 = st.columns(4)
+#
 
 OPTIONS = ["ax", "bx", "cx", "dx"]
 
 
-register = {"ax": 0000,
-            "bx": 0000,
-            "cx": 0000,
-            "dx": 0000}
+register = {"ax": "0000",
+            "bx": "0000",
+            "cx": "0000",
+            "dx": "0000"}
+
+pointers = {"sp": "0000",
+            "bp": "0000",
+            "si": "0000",
+            "dp": "0000"}
+
+
+def output(title: str, values: dict) -> None:
+    st.subheader(title)
+    for item, col in zip(values.keys(), st.columns(4)):
+        with col:
+            values[item] = st.text_input(f"{str(item).upper()}: ", value=values[item])
+
+
+output("Rejestry", register)
+
+output("Rejestry wskaźnikowe", pointers)
 
 
 with st.sidebar:
-    register["ax"] = col1.text_input("AX: ")
-    register["bx"] = col2.text_input("BX: ")
-    register["cx"] = col3.text_input("CX: ")
-    register["dx"] = col4.text_input("DX: ")
-
     mov_type = st.radio("TYPE: ", options=["mov", "xchg"])
 
     base_type = st.radio("TYPE: ", options=["bazowy", "indeksowy", "bazowo-indeksowy"])
@@ -33,8 +45,3 @@ with st.sidebar:
             tmp = register[dst_register]
             register[dst_register] = register[src_register]
             register[src_register] = tmp
-            
-        col1.text_input(label="OUTPUT AX", disabled=True, placeholder=register["ax"])
-        col2.text_input(label="OUTPUT BX", disabled=True, placeholder=register["bx"])
-        col3.text_input(label="OUTPUT CX", disabled=True, placeholder=register["cx"])
-        col4.text_input(label="OUTPUT DX", disabled=True, placeholder=register["dx"])
